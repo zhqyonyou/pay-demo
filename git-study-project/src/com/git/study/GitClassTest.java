aa574e93edc8a80e837621c5adab83ac9a50354a
17621761176 <1345395634@qq.com>
2020-12-01 14:14:19 +0800

    本地添加到本地git

---
+++ git-study-project/src/com/git/study/GitClassTest.java
@@ -0,0 +1,9 @@
+package com.git.study;
+
+/**
+ * @version GitClassTest v0.1 2020/12/1 14:06
+ * @Description
+ * @Author zhaoq
+ **/
+public class GitClassTest {
+}
---
+++ git-study-project/src/com/git/study/Main.java
@@ -0,0 +1,11 @@
+package com.git.study;
+
+/**
+ * @author zhaoq
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("git-study");
+    }
+}
